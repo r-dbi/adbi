f50899359107579b14a48516b58ddf7a8e656309
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2023-08-29 13:49:39 +0200

    rm skips

--- tests/testthat/helper-DBItest.R
+++ tests/testthat/helper-DBItest.R
@@ -9,17 +9,9 @@ DBItest::make_context(
   ),
   name = "adbi",
   default_skip = c(
-    # TODO: Remove when dbDisconnect() is implemented
-    "can_disconnect",
-    "disconnect_closed_connection",
-    "disconnect_invalid_connection",
-    # TODO: Remove when dbIsValid() is implemented
-    "is_valid_connection",
-    "is_valid_stale_connection",
     # TODO: Understand why test fails in R < 3.6
     if (getRversion() < "3.6") "connect_format",
     # Fails with older DBItest
-    if (packageVersion("DBItest") < "1.7.2") "reexport",
-    NULL
+    if (packageVersion("DBItest") < "1.7.2") "reexport"
   )
 )
