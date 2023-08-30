fea85fd03c61e3f70672830acde733c4542debee
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2023-08-30 11:07:30 +0200

    implement dbFetch

--- R/dbFetch_Result.R
+++ R/dbFetch_Result.R
@@ -2,17 +2,18 @@
 #' @inheritParams DBI::dbFetch
 #' @usage NULL
 dbFetch_AdbiResult <- function(res, n = -1, ...) {
-  #
-  # FOR IMPLEMENTERS:
-  #
-  # If you are interfacing against a native database library,
-  # consider copying and adapting Db*.cpp and Db*.h from r-dbi/RPostgres.
-  # These classes help growing a data frame whose number of rows
-  # may be unknown at the beginning.
-  # Similar code is in r-dbi/odbc.
-  #
-  testthat::skip("Not yet implemented: dbFetch(Result)")
+
+  if (!n == -1) {
+    testthat::skip("not implmented")
+  }
+
+  strm <- nanoarrow::nanoarrow_allocate_array_stream()
+
+  adbcdrivermanager::adbc_statement_execute_query(res@statement, stream = strm)
+
+  as.data.frame(strm)
 }
+
 #' @rdname DBI
 #' @export
 setMethod("dbFetch", "AdbiResult", dbFetch_AdbiResult)
--- tests/testthat/helper-DBItest.R
+++ tests/testthat/helper-DBItest.R
@@ -6,7 +6,15 @@ DBItest::make_context(
   tweaks = suppressWarnings(
     DBItest::tweaks(
       dbitest_version = "1.7.3",
-      constructor_relax_args = TRUE
+      constructor_relax_args = TRUE,
+      placeholder_pattern = c("?", "$1", "$name", ":name"),
+      date_cast = function(x) paste0("'", x, "'"),
+      time_cast = function(x) paste0("'", x, "'"),
+      timestamp_cast = function(x) paste0("'", x, "'"),
+      logical_return = function(x) as.numeric(x), # apache/arrow-adbc#1005
+      date_typed = FALSE,
+      time_typed = FALSE,
+      timestamp_typed = FALSE
     )
   ),
   name = "adbi"
