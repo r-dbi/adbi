6807ca512a33671054a6ffb769477223fa4abce9
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2023-09-28 11:00:49 +0200

    wip bind results

--- R/dbBind_Result.R
+++ R/dbBind_Result.R
@@ -2,7 +2,13 @@
 #' @inheritParams DBI::dbBind
 #' @usage NULL
 dbBind_AdbiResult <- function(res, params, ...) {
-  testthat::skip("Not yet implemented: dbBind(Result)")
+
+  # can we call this only once? if not, can we query the "prep" status?
+  adbcdrivermanager::adbc_statement_prepare(res@statement)
+  # any advantage of bind_stream over bind?
+  adbcdrivermanager::adbc_statement_bind(res@statement, params)
+
+  invisible(res)
 }
 #' @rdname DBI
 #' @export
