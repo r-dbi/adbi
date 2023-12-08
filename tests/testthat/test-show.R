8dd45e1836a1d3bf264f25c4503799e33e224c24
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2023-12-08 16:19:28 +0100

    add show tests

---
+++ tests/testthat/test-show.R
@@ -0,0 +1,25 @@
+test_that("show methods print", {
+
+  expect_output(show(adbi()))
+
+  drv <- adbi("adbcsqlite::adbcsqlite")
+
+  expect_output(show(drv))
+
+  con <- dbConnect(drv, uri = ":memory:")
+  withr::defer(dbDisconnect(con), priority = "last")
+
+  expect_output(show(con))
+
+  res <- dbSendQuery(con, "SELECT 1 AS a")
+  withr::defer(dbClearResult(res))
+
+  expect_output(show(res))
+
+  withr::deferred_clear()
+
+  expect_warning(dbDisconnect(con, force = TRUE))
+
+  expect_output(show(con))
+  expect_output(show(res))
+})
