e0947ad7fd3d44b4bcbe3660ac19636dc5f9c855
nbenn <nbenn@users.noreply.github.com>
2023-12-08 16:26:06 +0000

    Auto-update from GitHub Actions
    Run: https://github.com/r-dbi/adbi/actions/runs/7143739059

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,18 +1,18 @@
 test_that("dbDataType for blob", {
 
-	expect_identical(
-		db_data_type_blob(
-			structure(list(), class = "adbcsqlite_driver_sqlite")
-		),
-		"BLOB"
-	)
+  expect_identical(
+    db_data_type_blob(
+      structure(list(), class = "adbcsqlite_driver_sqlite")
+    ),
+    "BLOB"
+  )
 
-	expect_identical(
-		db_data_type_blob(
-			structure(list(), class = "adbcpostgresql_driver_postgresql")
-		),
-		"bytea"
-	)
+  expect_identical(
+    db_data_type_blob(
+      structure(list(), class = "adbcpostgresql_driver_postgresql")
+    ),
+    "bytea"
+  )
 
   expect_error(db_data_type_blob(structure(list(), class = "foo")))
 })
