e466961ebe20d9ad94fb5459f7938a64f2710ea4
Kirill Müller <kirill@cynkra.com>
2023-12-16 15:34:40 +0100

    Reexport

---
+++ R/export.R
@@ -0,0 +1,152 @@
+# Created with:
+# methods::getGenerics(asNamespace("DBI")) %>%
+#   grep("^db[A-Z]", ., value = TRUE) %>%
+#   setdiff(c("dbCallProc", "dbListConnections", "dbSetDataMappings", "dbGetException")) %>%
+#   paste0("#' @exportMethod ", ., "\nNULL\n", collapse = "\n") %>%
+#   cat(file = "R/export.R")
+#' @exportMethod dbAppendTable
+NULL
+
+#' @exportMethod dbAppendTableArrow
+NULL
+
+#' @exportMethod dbBegin
+NULL
+
+#' @exportMethod dbBind
+NULL
+
+#' @exportMethod dbBindArrow
+NULL
+
+#' @exportMethod dbCanConnect
+NULL
+
+#' @exportMethod dbClearResult
+NULL
+
+#' @exportMethod dbColumnInfo
+NULL
+
+#' @exportMethod dbCommit
+NULL
+
+#' @exportMethod dbConnect
+NULL
+
+#' @exportMethod dbCreateTable
+NULL
+
+#' @exportMethod dbCreateTableArrow
+NULL
+
+#' @exportMethod dbDataType
+NULL
+
+#' @exportMethod dbDisconnect
+NULL
+
+#' @exportMethod dbDriver
+NULL
+
+#' @exportMethod dbExecute
+NULL
+
+#' @exportMethod dbExistsTable
+NULL
+
+#' @exportMethod dbFetch
+NULL
+
+#' @exportMethod dbFetchArrow
+NULL
+
+#' @exportMethod dbFetchArrowChunk
+NULL
+
+#' @exportMethod dbGetConnectArgs
+NULL
+
+#' @exportMethod dbGetInfo
+NULL
+
+#' @exportMethod dbGetQuery
+NULL
+
+#' @exportMethod dbGetQueryArrow
+NULL
+
+#' @exportMethod dbGetRowCount
+NULL
+
+#' @exportMethod dbGetRowsAffected
+NULL
+
+#' @exportMethod dbGetStatement
+NULL
+
+#' @exportMethod dbHasCompleted
+NULL
+
+#' @exportMethod dbIsReadOnly
+NULL
+
+#' @exportMethod dbIsValid
+NULL
+
+#' @exportMethod dbListFields
+NULL
+
+#' @exportMethod dbListObjects
+NULL
+
+#' @exportMethod dbListResults
+NULL
+
+#' @exportMethod dbListTables
+NULL
+
+#' @exportMethod dbQuoteIdentifier
+NULL
+
+#' @exportMethod dbQuoteLiteral
+NULL
+
+#' @exportMethod dbQuoteString
+NULL
+
+#' @exportMethod dbReadTable
+NULL
+
+#' @exportMethod dbReadTableArrow
+NULL
+
+#' @exportMethod dbRemoveTable
+NULL
+
+#' @exportMethod dbRollback
+NULL
+
+#' @exportMethod dbSendQuery
+NULL
+
+#' @exportMethod dbSendQueryArrow
+NULL
+
+#' @exportMethod dbSendStatement
+NULL
+
+#' @exportMethod dbUnloadDriver
+NULL
+
+#' @exportMethod dbUnquoteIdentifier
+NULL
+
+#' @exportMethod dbWithTransaction
+NULL
+
+#' @exportMethod dbWriteTable
+NULL
+
+#' @exportMethod dbWriteTableArrow
+NULL
