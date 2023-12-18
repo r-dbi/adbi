21791fd43b50542e82f0e38ec3e27d65afb6c8dc
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2023-12-18 11:12:14 +0100

    improve dbWriteTable() errors

--- R/dbWriteTable_AdbiConnection_Id_data.frame.R
+++ R/dbWriteTable_AdbiConnection_Id_data.frame.R
@@ -4,7 +4,7 @@
 #'   `TRUE` if `append` is also `TRUE`.
 #' @param append Allow appending to the destination table. Cannot be
 #'   `TRUE` if `overwrite` is also `TRUE`.
-#' @param field.types character vector of named  SQL field types where
+#' @param field.types character vector of named SQL field types where
 #'   the names are the names of new table's columns. If missing, types inferred
 #'   with [DBI::dbDataType()]).
 #' @param row.names A logical specifying whether the `row.names` should be
@@ -15,8 +15,9 @@
 #' @param temporary a logical specifying whether the new table should be
 #'   temporary. Its default is `FALSE`.
 #' @usage NULL
-dbWriteTable_AdbiConnection_Id_data.frame <- function(conn, name, value, overwrite = FALSE, append = FALSE, ..., field.types = NULL, row.names = NULL,
-    temporary = FALSE) {
+dbWriteTable_AdbiConnection_Id_data.frame <- function(conn, name, value,
+    overwrite = FALSE, append = FALSE, ..., field.types = NULL,
+    row.names = NULL, temporary = FALSE) {
 
   if (!dbIsValid(conn)) {
     stop("Invalid connection.", call. = FALSE)
@@ -29,37 +30,33 @@ dbWriteTable_AdbiConnection_Id_data.frame <- function(conn, name, value, overwri
   if ((!is.logical(row.names) && !is.character(row.names)) ||
     length(row.names) != 1L) {
 
-    stop("`row.names` must be a logical scalar or a string")
+    stop(
+      "Argument `row.names` must be a logical scalar or a string",
+      call. = FALSE
+    )
   }
 
   if (!is.logical(overwrite) || length(overwrite) != 1L || is.na(overwrite)) {
-    stop("`overwrite` must be a logical scalar")
+    stop("Argument `overwrite` must be a logical scalar", call. = FALSE)
   }
 
   if (!is.logical(append) || length(append) != 1L || is.na(append)) {
-    stop("`append` must be a logical scalar")
+    stop("Argument `append` must be a logical scalar", call. = FALSE)
   }
 
   if (!is.logical(temporary) || length(temporary) != 1L) {
-    stop("`temporary` must be a logical scalar")
+    stop("Argument `temporary` must be a logical scalar", call. = FALSE)
   }
 
   if (overwrite && append) {
-    stop("overwrite and append cannot both be TRUE")
+    stop(
+      "Arguments `overwrite` and `append` cannot both be TRUE",
+      call. = FALSE
+    )
   }
 
-  if (!is.null(field.types) && !(is.character(field.types) &&
-    !is.null(names(field.types)) && !anyDuplicated(names(field.types)))) {
-
-    stop("`field.types` must be a named character vector with unique names, ",
-      "or NULL")
-
-  } else if (!is.null(field.types)) {
-    stop("specifying field.types is not supported")
-  }
-
-  if (append && !is.null(field.types)) {
-    stop("Cannot specify `field.types` with `append = TRUE`")
+  if (!is.null(field.types)) {
+    stop("Specifying field.types is not supported", call. = FALSE)
   }
 
   catalog <- NULL
