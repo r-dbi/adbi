3598c45a1ab2dcbf258a0a8edc056442d06ab90e
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2023-12-18 19:31:27 +0100

    rm redundant re-exports

--- R/AdbiConnection.R
+++ R/AdbiConnection.R
@@ -48,36 +48,6 @@ setClass(
   contains = "DBIConnection"
 )
 
-#' @export
-DBI::dbIsReadOnly
-
-#' @export
-DBI::dbQuoteLiteral
-
-#' @export
-DBI::dbUnquoteIdentifier
-
-#' @export
-DBI::dbGetQuery
-
-#' @export
-DBI::dbExecute
-
-#' @export
-DBI::dbReadTable
-
-#' @export
-DBI::dbCreateTable
-
-#' @export
-DBI::dbAppendTable
-
-#' @export
-DBI::dbListObjects
-
-#' @export
-DBI::dbWithTransaction
-
 bigint_opts <- c(
   "integer",
   "integer-strict",
